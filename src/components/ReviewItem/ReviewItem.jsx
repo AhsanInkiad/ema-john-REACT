a5f1869c6cf12e242e7ee6c1ea5356ab3be039e1
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2024-05-24 19:33:07 +0600

    Added delete button in Orders page. And made data consistent across UI.

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './ReviewItem.css';
 
-const ReviewItem = ({product}) => {
-    const {id, img, name, quantity} = product;
+const ReviewItem = ({product, handleRemoveFromCart}) => {
+    const {id, img, price, name, quantity} = product;
     return (
         <div className='review-item'>
            <img src={img} alt="" />
+           <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order quantity: <span className='orange-text'>{quantity}</span> </p>
+           </div>
+           <button onClick={()=>handleRemoveFromCart(id)} className='d-btn'>Delete</button>
         </div>
     );
 };
